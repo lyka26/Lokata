adddb2e835bee8ee28228619b21c1a81984fe8d2
lyka26 <lykammarcellano@gmail.com>
2023-08-06 18:15:28 +0800

    login and sign up

---
+++ app/src/main/java/com/example/lokata/Generic/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.lokata.Generic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lokata.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
---
+++ app/src/main/java/com/example/lokata/Generic/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.lokata.Generic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lokata.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
